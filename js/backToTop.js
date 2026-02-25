0dba51620d52672544924d1c06ff63a4e7dd5c5a
Patrycja <patkadwa@gmail.com>
2026-02-26 00:04:28 +0100

    Update site content and add new media

--- js/backToTop.js
+++ js/backToTop.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const topButton = document.querySelector(".top_button");
 
     window.addEventListener("scroll", () => {
-        if (window.scrollY > 20) { // zmień wartość, jeśli chcesz inną wysokość
+        if (window.scrollY > 20) { 
             topButton.classList.add("visible");
         } else {
             topButton.classList.remove("visible");
